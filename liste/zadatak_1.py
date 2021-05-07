607de13efa11afbc629f30cea05783fd81ead921
Boban Stosic <lelehip@gmail.com>
2021-05-07 18:06:12 +0200

    ispis osnovne liste

--- liste/zadatak_1.py
+++ liste/zadatak_1.py
@@ -1,9 +1,8 @@
 L = eval(input('Unesite niz celih brojeva: '))
-print(L)
+print('Vasa lista:', (L))
 print('Zbir elemenata vasega niza je: ', sum(L))
 print('U vasem nizu ima: ', len(L), 'elemenata.')
 print('Vasa lista unazad: ', L[::-1])
 
-#nesto nvo za git
 
 
