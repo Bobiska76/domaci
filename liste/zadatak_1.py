34935768c2c6ea13c71cb9b9a5ec6d73c4f294a8
Boban Stosic <lelehip@gmail.com>
2021-05-10 09:31:09 +0200

    dodao fajl sa domacim

--- liste/zadatak_1.py
+++ liste/zadatak_1.py
@@ -4,6 +4,7 @@ print('Zbir elemenata vasega niza je: ', sum(L))
 print('U vasem nizu ima: ', len(L), 'elemenata.')
 print('Vasa lista unazad: ', L[::-1])
 
-#nesto nvo za git
+
+
 
 
---
+++ stringovi/domaci.py
@@ -0,0 +1,51 @@
+####################Prvi zadatak##################
+
+s = input('Unesite neki string: ')
+
+# print('Vas string ima:',len(s), 'karaktera')
+# print((s + ' ')* 10)
+# print(s[0])
+# print(s[:3])
+# print(s[-3:])
+# print(s[::-1])
+# if len(s) < 7:
+#     print('Vas string nema dovoljnu duzinu. Minimum je sedam karaktera')
+# else:
+#     print(s[6])
+# print(s[1 : -1])
+# print(s.upper())
+# print(s.replace('a', 'e'))
+# •	String u kome je svako slovo ( alpha znak ) zamenjeno sa blanko znakom
+
+###############################Drugi zadatak########################################
+
+#Jedan jednostavan način da se proceni broj reči u stringu
+#je da se prebroji broj blanko znakova (space) u stringu. 
+# Napišite program koji traži od korisnika da unese tekst, 
+# a onda mu vraća procenjeni broj reči u tom stringu.
+
+#############################Treci Zadatak##################################
+
+#Ljudi često zaborave da zatvore zagradu u formulama. 
+# Napišite program koji traži od korisnika da unese formulu i štampa poruku 
+# da li formula ima isti broj levih i desnih zagrada.
+
+######################Zadatak 4##############################################
+
+
+
+######################Zadatak 5##############################################
+
+######################Zadatak 6##############################################
+
+######################Zadatak 7##############################################
+
+######################Zadatak 8##############################################
+
+######################Zadatak 9##############################################
+
+######################Zadatak 10##############################################
+
+
+
+
